4c6d5817c873ecf73e46df9f65b1e9a792728d7a
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2022-07-19 20:15:23 +0200

    Add Group line plot

--- game_statistics/game_statistics.py
+++ game_statistics/game_statistics.py
@@ -1,9 +1,10 @@
 import os
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import plotly.express as px
 import seaborn as sns
 
 from monopoly.board.board import Board
@@ -34,18 +35,22 @@ class GameStatistics:
             rounds (int, optional): Number of rounds (Crossing the GO tile). 
             Defaults to 10000.
         """
-        # Statistics Related Attributes
-        self.__stats: Dict[str, int] = {}
-        self.__rounds: int = rounds
-
-        # Player Related Attributes
-        self.__player: Player = Player()
-
         # Board Related Attributes
         self.__board: Board = Board(file=board_data)
         self.__chances: Deck = Deck(file=chances_data)
         self.__community_chests: Deck = Deck(file=community_chests_data)
+
+        # Statistics Related Attributes
+        self.__stats: Dict[str, int] = {}
         self.__load_tile_names()
+        self.__rounds: int = rounds
+        self.__data: pd.DataFrame = pd.DataFrame.from_dict(
+            self.__stats,
+            orient='index'
+        )
+
+        # Player Related Attributes
+        self.__player: Player = Player()
 
         # Output Related Attributes
         self.__output_file: str = output_file
@@ -58,13 +63,93 @@ class GameStatistics:
 
     def __run(self) -> None:
         """Simulate the Game of Monopoly."""
+        round_number: int = 0
+
         while self.__player.crossed_go_tile < self.__rounds:
+
             self.__player.execute_round(
                 board=self.__board,
                 chances=self.__chances,
                 community_chests=self.__community_chests,
                 stats=self.__stats
             )
+
+            self.__data[f'Round {round_number}'] = pd.Series(self.__stats)
+
+            if self.__player.crossed_go_tile != round_number:
+                round_number += 1
+
+        groups = [x.split('#')[0].strip() for x in self.__data.index.values]
+
+        self.__data['Group'] = groups
+        # self.__data.drop(0, axis=1, inplace=True)
+        self.__data.reset_index(inplace=True)
+
+        simplified_data: pd.DataFrame = (
+            self.__data.groupby('Group').transform('sum')
+        )
+
+        simplified_data.drop_duplicates(inplace=True)
+
+        simplified_data['index'] = [
+            x.split('#')[0].strip() for x in simplified_data['index'].values
+        ]
+
+        simplified_data = simplified_data.transpose().reset_index(drop=True)
+        simplified_data.columns = simplified_data.iloc[0].values
+        simplified_data.drop(labels=0, axis=0, inplace=True)
+        simplified_data.drop(
+            labels=[
+                'Go', 'Jail', 'Visiting Jail', 'Free Parking', 'Go To Jail'
+            ],
+            axis=1,
+            inplace=True
+        )
+
+        print(simplified_data)
+
+        plt.figure(figsize=(25, 15))
+        fig = px.line(
+            simplified_data,
+            x=simplified_data.index,
+            y=simplified_data.columns,
+            title='Title'
+        )
+        fig.show()
+
+        fig.write_html(
+            os.path.join(
+                os.getcwd(),
+                'output',
+                'plots',
+                'line_charts',
+                f'monopoly_game_category_visit_{self.__rounds}_rounds_'
+                f'{self.__timestamp}.html'
+            )
+        )
+
+        # fig = plt.figure()
+        # ax = plt.subplot(111)
+
+        # simplified_data.plot.line()
+
+        # # Shrink current axis by 20%
+        # box = ax.get_position()
+        # ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+        # # Put a legend to the right of the current axis
+        # ax.legend(loc='center right', bbox_to_anchor=(1, 0.5))
+
+        # plt.savefig(
+        # os.path.join(
+        #     os.getcwd(),
+        #     'output',
+        #     'plots',
+        #     'line_charts',
+        #     f'monopoly_game_category_visit_{self.__rounds}_rounds_'
+        #     f'{self.__timestamp}.png'
+        # )
+        # )
 
     def __load_data_to_numpy_array(self) -> np.ndarray:
         """Load Statistic Data to NumPy Array for Heatmap.
@@ -215,5 +300,5 @@ class GameStatistics:
         self.__save_statistics()
 
         # Generate Plots
-        self.__generate_barplots()
-        self.__generate_heatmap()
+        # self.__generate_barplots()
+        # self.__generate_heatmap()
