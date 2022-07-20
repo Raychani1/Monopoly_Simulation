2db4634bc506990a4db4617ff27d851731f4cd20
Ladislav Rajcsányi <rajcsanyi.ladislav.it@gmail.com>
2022-07-20 18:19:28 +0200

    Add Plotly plots (#5)
    * Add Group line plot
    
    * Add config file
    
    * Add Plotly plots

---
+++ game_statistics/config.py
@@ -0,0 +1,13 @@
+from typing import Dict, List
+
+group_drop_columns: List[str] = [
+    'Go', 'Jail', 'Visiting Jail', 'Free Parking', 'Go To Jail'
+]
+
+line_chart_labels: Dict[str, str] = {
+    'index': 'Round',
+    'value': 'Number of Visits',
+    'variable': 'Group'
+}
+
+top_10_columns: List[str] = ['Tile', 'Number of Visits']
--- game_statistics/game_statistics.py
+++ game_statistics/game_statistics.py
@@ -1,11 +1,16 @@
 import os
 from typing import Any, Dict
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
+import plotly.express as px
+import plotly.figure_factory as ff
 
+from game_statistics.config import (
+    group_drop_columns,
+    line_chart_labels,
+    top_10_columns
+)
 from monopoly.board.board import Board
 from monopoly.deck.deck import Deck
 from monopoly.player.player import Player
@@ -31,21 +36,28 @@ class GameStatistics:
             chances_data (str): Chance tiles data file path.
             community_chests_data (str): Community Chest tiles data file path.
             output_file (str): Game Statistics output file path.
-            rounds (int, optional): Number of rounds (Crossing the GO tile). 
+            rounds (int, optional): Number of rounds (Crossing the GO tile).
             Defaults to 10000.
         """
-        # Statistics Related Attributes
-        self.__stats: Dict[str, int] = {}
-        self.__rounds: int = rounds
-
-        # Player Related Attributes
-        self.__player: Player = Player()
-
         # Board Related Attributes
         self.__board: Board = Board(file=board_data)
         self.__chances: Deck = Deck(file=chances_data)
         self.__community_chests: Deck = Deck(file=community_chests_data)
+
+        # Statistics Related Attributes
+        self.__stats: Dict[str, int] = {}
         self.__load_tile_names()
+        self.__rounds: int = rounds
+        self.__data: pd.DataFrame = pd.DataFrame.from_dict(
+            self.__stats,
+            orient='index'
+        )
+        self.__heatmap_label_mapping: pd.DataFrame = (
+            pd.DataFrame(np.empty((11, 11), dtype=np.str))
+        )
+
+        # Player Related Attributes
+        self.__player: Player = Player()
 
         # Output Related Attributes
         self.__output_file: str = output_file
@@ -58,13 +70,21 @@ class GameStatistics:
 
     def __run(self) -> None:
         """Simulate the Game of Monopoly."""
+        round_number: int = 0
+
         while self.__player.crossed_go_tile < self.__rounds:
+
             self.__player.execute_round(
                 board=self.__board,
                 chances=self.__chances,
                 community_chests=self.__community_chests,
                 stats=self.__stats
             )
+
+            self.__data[f'Round {round_number}'] = pd.Series(self.__stats)
+
+            if self.__player.crossed_go_tile != round_number:
+                round_number += 1
 
     def __load_data_to_numpy_array(self) -> np.ndarray:
         """Load Statistic Data to NumPy Array for Heatmap.
@@ -85,36 +105,67 @@ class GameStatistics:
                 self.__stats[self.__board.tiles[row[0]].label]
             )
 
+            self.__heatmap_label_mapping.at[
+                row[1], row[2]
+            ] = self.__board.tiles[row[0]].label
+
         return heatmap_data.astype(int)
+
+    def __generate_line_chart(self) -> None:
+        """Generate and Save 'Group Visit' line chart."""
+        self.__process_round_data()
+
+        fig = px.line(
+            self.__data,
+            x=self.__data.index,
+            y=self.__data.columns,
+            title=f'Visit by Group - {self.__rounds} Rounds',
+            labels=line_chart_labels,
+        )
+
+        fig.update_traces(mode='lines', hovertemplate=None)
+        fig.update_layout(hovermode='x unified')
+
+        fig.show()
+
+        fig.write_html(
+            os.path.join(
+                os.getcwd(),
+                'output',
+                'plots',
+                'line_charts',
+                f'monopoly_game_category_visit_{self.__rounds}_rounds_'
+                f'{self.__timestamp}.html'
+            )
+        )
 
     def __generate_roll_barplot(self) -> None:
         """Generate and Save 'Roll Distribution' barplot."""
         data: pd.DataFrame = pd.DataFrame(
             self.__player.roll_history,
             columns=['Rolls']
-        )
+        ).sort_values(by='Rolls')
 
-        sns.displot(
+        fig = px.histogram(
             data,
-            x="Rolls",
-            height=10,
-            bins=data['Rolls'].nunique()
+            x='Rolls',
+            color='Rolls',
+            title=f'Rolls of 1 Player - {self.__rounds} Rounds',
+            text_auto=True,
+
         )
+        fig.update_layout(bargap=0.2, legend_traceorder='normal')
 
-        plt.title(
-            f'Rolls of 1 Player - {self.__rounds} Rounds'
-        )
+        fig.show()
 
-        plt.tight_layout()
-
-        plt.savefig(
+        fig.write_html(
             os.path.join(
                 os.getcwd(),
                 'output',
                 'plots',
                 'barplots',
                 f'monopoly_game_rolls_barplot_{self.__rounds}_rounds_'
-                f'{self.__timestamp}.png'
+                f'{self.__timestamp}.html'
             )
         )
 
@@ -127,31 +178,30 @@ class GameStatistics:
 
         data.reset_index(inplace=True)
 
-        data.columns = ['Tile', 'Number of Visits']
+        data.columns = top_10_columns
 
-        plt.figure(figsize=(16, 9))
-        sns.barplot(
-            data=data.head(10),
+        data.sort_values(by='Number of Visits', ascending=False, inplace=True)
+
+        fig = px.histogram(
+            data.head(10),
             x='Tile',
             y='Number of Visits',
+            title=f'Top 10 Tiles Visited by 1 Player - {self.__rounds} Rounds',
+            color='Number of Visits',
+            text_auto=True
         )
+        fig.update_layout(bargap=0.2, yaxis_title='Number of Visits')
 
-        plt.title(
-            f'Top 10 Tiles Visited by 1 Player - {self.__rounds} Rounds'
-        )
+        fig.show()
 
-        plt.xticks(rotation=30)
-
-        plt.tight_layout()
-
-        plt.savefig(
+        fig.write_html(
             os.path.join(
                 os.getcwd(),
                 'output',
                 'plots',
                 'barplots',
-                f'monopoly_top_10_tiles_barplot_{self.__rounds}_rounds_'
-                f'{self.__timestamp}.png'
+                f'monopoly_game_rolls_barplot_{self.__rounds}_rounds_'
+                f'{self.__timestamp}.html'
             )
         )
 
@@ -163,30 +213,78 @@ class GameStatistics:
     def __generate_heatmap(self) -> None:
         """Generate and Save Monopoly Board Heatmap."""
         data: np.ndarray = self.__load_data_to_numpy_array()
+        data = data[::-1]
 
-        fig = plt.figure(figsize=(16, 9))
-        ax = sns.heatmap(data, annot=True, linewidth=0.5, fmt='d')
-        fig.add_axes(ax)
-        plt.title(
-            f'Monopoly Board Heatmap of 1 Player - {self.__rounds} Rounds'
+        annotations = data.copy()
+        annotations = np.where(annotations == 0, '', annotations)
+
+        fig = ff.create_annotated_heatmap(
+            data,
+            annotation_text=annotations,
         )
 
-        for t in ax.texts:
-            if int(t.get_text()) > 0:
-                t.set_text(t.get_text())
-            else:
-                t.set_text("")
+        fig.update(
+            data=[{
+                'customdata': self.__heatmap_label_mapping.to_numpy()[::-1],
+                'hovertemplate': (
+                    'Tile: %{customdata}<br>'
+                    'Number of Visits: %{z}<extra></extra>'
+                )
+            }]
+        )
+        fig.update_layout(
+            xaxis_visible=False,
+            xaxis_showticklabels=False,
+            yaxis_visible=False,
+            yaxis_showticklabels=False,
+            title=(
+                'Monopoly Board Heatmap of 1 Player - '
+                f'{self.__rounds} Rounds'
+            )
+        )
 
-        plt.savefig(
+        fig.show()
+
+        fig.write_html(
             os.path.join(
                 os.getcwd(),
                 'output',
                 'plots',
                 'heatmaps',
                 f'monopoly_board_heatmap_{self.__rounds}_rounds_'
-                f'{self.__timestamp}.png'
+                f'{self.__timestamp}.html'
             )
         )
+
+    def __process_round_data(self) -> None:
+        """Process accumulated round visit Data."""
+        groups = [x.split('#')[0].strip() for x in self.__data.index.values]
+
+        self.__data['Group'] = groups
+
+        self.__data.reset_index(inplace=True)
+
+        self.__data = (
+            self.__data.groupby('Group').transform('sum')
+        )
+
+        self.__data.drop_duplicates(inplace=True)
+
+        self.__data['index'] = [
+            x.split('#')[0].strip() for x in self.__data['index'].values
+        ]
+
+        self.__data = self.__data.transpose().reset_index(drop=True)
+        self.__data.columns = self.__data.iloc[0].values
+        self.__data.drop(labels=0, axis=0, inplace=True)
+        self.__data.drop(
+            labels=group_drop_columns,
+            axis=1,
+            inplace=True
+        )
+
+        self.__data = self.__data.reindex(sorted(self.__data.columns), axis=1)
+        self.__data = self.__data.reset_index(drop=True)
 
     def __process_statistics(self) -> None:
         """Sort Statistics."""
@@ -216,4 +314,5 @@ class GameStatistics:
 
         # Generate Plots
         self.__generate_barplots()
+        self.__generate_line_chart()
         self.__generate_heatmap()
